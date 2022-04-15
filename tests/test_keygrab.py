f626cc4d7446bb03b3175fd7ad8af288b176768c
Davis <obviouspy@gmail.com>
2022-04-15 12:46:49 -0400

    Remove invalid test

--- tests/test_keygrab.py
+++ tests/test_keygrab.py
@@ -4,21 +4,10 @@ from keywatch import Keygrab
 from utils import SafetyNet
 from general_tests import test_single_bind, test_unbind_rebind, test_expect_double_bind_error, test_grab_grabbed_listener
 
-def test_expect_grab_error(listener: Keygrab):
-	with SafetyNet(listener) as k:
-		try:
-			k._grab(0)
-			k._grab(0)
-		except Exception:
-			pass
-		else:
-			raise Exception('Expected an error. Grabbed the keycode "0" twice in a row.')
-
 def main():
 	queue = Queue()
 	k = Keygrab()
 
-	test_expect_grab_error(Keygrab())
 	test_unbind_rebind(k, queue, 'b')
 	test_single_bind(k, queue, 'a')
 	test_single_bind(k, queue, 'b')
